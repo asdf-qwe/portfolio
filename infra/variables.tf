b583a8b53873dc1d37f580d4e33747a701b4f6f9
asdf-qwe <fism@naver.com>
2025-09-17 19:50:14 +0900

    wip0917

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "dev3"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "asdf-qwe"
+}
