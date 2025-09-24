def3faf22bcd79c7d9148ee27663b65559c4a5bf
asdf-qwe <fism@naver.com>
2025-09-25 00:53:55 +0900

    text

--- backend/src/main/java/com/port/folio/domain/main/entity/Main.java
+++ backend/src/main/java/com/port/folio/domain/main/entity/Main.java
@@ -2,6 +2,7 @@ package com.port.folio.domain.main.entity;
 
 import com.port.folio.domain.user.entity.User;
 import com.port.folio.global.Jpa.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
@@ -18,6 +19,7 @@ public class Main extends BaseEntity {
 
     private String greeting;
     private String smallGreeting;
+    @Column(columnDefinition = "TEXT")
     private String introduce;
     private String name;
     private String job;
